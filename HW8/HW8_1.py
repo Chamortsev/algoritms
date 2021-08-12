155b52188c343f9bb82d6b56cd90717929a0ee01
chamorcev <chamorsev2mail.ru>
2021-08-12 16:46:41 +0300

    HW8 Вторую задачу пока не осилил, постараюсь доделать.

---
+++ HW8/HW8_1.py
@@ -0,0 +1,19 @@
+# 1. Определение количества различных подстрок с использованием хеш-функции.
+# Пусть дана строка S длиной N. Например, состоящая только из маленьких латинских букв.
+# Требуется найти количество различных подстрок в этой строке.
+# Для решения задачи рекомендую воспользоваться алгоритмом sha1 из модуля hashlib или встроенную функцию hash()
+
+import hashlib
+
+
+def under_string(inp):
+    user_input = str(inp).lower()
+    inp_len = len(user_input)
+    hash_set = set()
+    for i in range(inp_len + 1):
+        for j in range(i + 1, inp_len + 1):
+            h = hashlib.sha1(user_input[i:j].encode('utf-8')).hexdigest()
+            hash_set.add(h)
+    return len(hash_set)
+inp_len = 'hello'
+print(f'К-во подстрок в строке {inp_len}: {under_string(inp_len)}')
